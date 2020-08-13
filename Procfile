40d803455918bd1efa87d0a22b417e2fad662cc4
Sudharsan <sudharsanj286@gmail.com>
2020-08-13 20:27:08 +0530

    update changes for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kdud.wsgi --log-file -
--- sid/settings.py
+++ sid/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'qsbkn*@mmoyk92=6n@813o7d$ymafax0qgcc4np!@(6ccziphm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
