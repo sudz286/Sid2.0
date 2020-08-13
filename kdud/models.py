6fc05859c54afc93fb89caf5cc1d0f89cd33c417
Sudharsan <sudharsanj286@gmail.com>
2020-08-13 20:19:31 +0530

    update PC pages

--- kdud/models.py
+++ kdud/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-
 # Create your models here.
 # class TourInfo(models.Model):
 	
-	
+class TourModel(models.Model):
+	time = models.CharField(max_length = 20)
+	dates = models.CharField(max_length = 50)
+	venue = models.CharField(max_length = 50)
+	band = models.CharField(max_length = 50)
+	def __str__(self):
+		return self.dates
--- kdud/views.py
+++ kdud/views.py
@@ -1,16 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.shortcuts import render
-
+from .models import TourModel
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-class TourPageView(TemplateView):
+class TourPageView(ListView):
     template_name = "tour.html"
+    model = TourModel
+    context_object_name = "tour_info"
 
 class StoryPageView(TemplateView):
-    template_name = "home.html"
+    template_name = "story.html"
 
 class ContactPageView(TemplateView):
     template_name = "contact.html"
